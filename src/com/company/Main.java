b9ec02fc8f9a09698d3ab18fc5360fbc20018c3a
monpeco <monpeco@gmail.com>
2017-02-08 23:06:59 -0300

    Update class to play with variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,11 @@
 package com.company;
 
 public class Main {
+    int num = 0;
+    static String name;
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.print("hallo");
+	    double result = 0;
+        System.out.print("hallo " + result);
     }
 }
