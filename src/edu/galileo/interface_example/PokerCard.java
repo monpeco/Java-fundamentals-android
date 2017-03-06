29688c247ee616442650b851ab38c645eac02c48
monpeco <monpeco@gmail.com>
2017-03-05 21:04:27 -0300

    Add comment on suit and number convention

--- src/edu/galileo/interface_example/PokerCard.java
+++ src/edu/galileo/interface_example/PokerCard.java
@@ -6,8 +6,8 @@ import java.util.Comparator;
  * Created by M on 05-03-2017.
  */
 public class PokerCard implements Comparable<PokerCard> {
-    int suit;
-    int number;
+    int suit;       //4-spades, 3-hearts, 2-clubs, 1-diamods
+    int number;     //from 2 to 14, begin 11 the jack, 12 the queen, 13 the king and 14 the ace
 
     public PokerCard(int s, int n){
         suit = s;
